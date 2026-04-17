3c73fdebe0619126018f330d83f40264b1367b48
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2026-04-17 19:34:09 +0300

    Delete lib directory

--- lib/utils.ts
+++
@@ -1,9 +0,0 @@
-export const currency = new Intl.NumberFormat('ar-SA', {
-  style: 'currency',
-  currency: 'SAR',
-  maximumFractionDigits: 0
-});
-
-export function cn(...classes: Array<string | undefined | false>) {
-  return classes.filter(Boolean).join(' ');
-}
